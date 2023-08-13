2a518fc1b743e8ae012dab71c76624d3c4d410bb
kingslayerrq <ruizhequ813@gmail.com>
2023-08-12 23:23:40 -0400

    initial commit

---
+++ src/testFrame.java
@@ -0,0 +1,32 @@
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
+public class testFrame extends JFrame {
+
+	/**
+	 * Launch the application.
+	 */
+//	public static void main(String[] args) {
+//		EventQueue.invokeLater(new Runnable() {
+//			public void run() {
+//				try {
+//					testFrame frame = new testFrame();
+//					frame.setVisible(true);
+//				} catch (Exception e) {
+//					e.printStackTrace();
+//				}
+//			}
+//		});
+//	}
+
+	/**
+	 * Create the frame.
+	 */
+	public testFrame() {
+		setBounds(100, 100, 450, 300);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+	}
+
+}
